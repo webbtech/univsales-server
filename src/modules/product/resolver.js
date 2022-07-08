184b96cfbdf8c40a66c79e6cb3b22c23abe8d007
Ron Dyck <rond@webbtech.net>
2022-07-08 13:14:48 -0400

    test updates

--- src/modules/product/resolver.js
+++ src/modules/product/resolver.js
@@ -2,6 +2,31 @@
 
 import Product from './model'
 
+const productPersist = async (input) => {
+  try {
+    const ret = await Product.findOneAndUpdate(
+      { _id: input._id },
+      input,
+      { new: true },
+    )
+    return ret
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+
+const findOne = async (productID) => {
+  if (!productID) {
+    throw new Error('Missing product id in product query')
+  }
+  try {
+    const product = await Product.findById(productID)
+    return product
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+
 const find = async () => {
   try {
     const items = await Product.find({}).sort({ name: 1 })
@@ -12,7 +37,11 @@ const find = async () => {
 }
 
 export const resolvers = {
+  Mutation: {
+    productPersist: (_, { input }) => productPersist(input),
+  },
   Query: {
+    product: (_, { productID }) => findOne(productID),
     products: () => find(),
   },
 }
